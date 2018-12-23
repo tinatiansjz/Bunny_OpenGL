234dde157a7a051f04e08467928b874a56bbe5e7
Tina <595101270@qq.com>
2018-12-23 21:53:28 +0800

    add flash light & point light's attenuation

--- bunny_new/bunny.fs
+++ bunny_new/bunny.fs
@@ -4,40 +4,110 @@ out vec4 FragColor;
 struct Material {
     vec3 ambient;
     vec3 diffuse;
-    vec3 specular;    
+    vec3 specular;
     float shininess;
 }; 
 
-struct Light {
+struct PointLight {
     vec3 position;
+    
+    float constant;
+    float linear;
+    float quadratic;
+	
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
 };
 
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+  
+    float constant;
+    float linear;
+    float quadratic;
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;       
+};
+
 in vec3 FragPos;
 in vec3 Normal;
-  
-uniform vec3 viewPos;
-uniform Material material;
-uniform Light light;
 
-void main(){
-    // ambient
-    vec3 ambient = light.ambient * material.ambient;
-  	
-    // diffuse 
+uniform vec3 viewPos;
+uniform PointLight pointLights;
+uniform SpotLight spotLight;
+uniform Material material;
+
+// function prototypes
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
+void main(){    
+    // properties
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
-    
-    // specular
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
-        
-    vec3 result = ambient + diffuse + specular;
+    
+    // == =====================================================
+    // Our lighting is set up in 2 phases: a point light and a flashlight
+    // For each phase, a calculate function is defined that calculates the corresponding color
+    // per lamp. In the main() function we take all the calculated colors and sum them up for
+    // this fragment's final color.
+    // == =====================================================
+    // phase 1: point lights
+    vec3 result = CalcPointLight(pointLights, norm, FragPos, viewDir);    
+    // phase 2: spot light
+    result += CalcSpotLight(spotLight, norm, FragPos, viewDir);    
+    
     FragColor = vec4(result, 1.0);
-} 
+}
+
+// calculates the color when using a point light.
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir){
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+    // combine results
+    vec3 ambient = light.ambient * material.ambient;
+    vec3 diffuse = light.diffuse * diff * material.diffuse;
+    vec3 specular = light.specular * spec * material.specular;
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+}
+
+// calculates the color when using a spot light.
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir){
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+    // spotlight intensity
+    float theta = dot(lightDir, normalize(-light.direction)); 
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    // combine results
+    vec3 ambient = light.ambient * material.diffuse;
+    vec3 diffuse = light.diffuse * material.diffuse;
+    vec3 specular = light.specular * spec * material.specular;
+    ambient *= attenuation * intensity;
+    diffuse *= attenuation * intensity;
+    specular *= attenuation * intensity;
+    return (ambient + diffuse + specular);
+}
--- bunny_new/main.cpp
+++ bunny_new/main.cpp
@@ -10,11 +10,12 @@
 
 #include <iostream>
 #include "parser.h"
-
+#define INF 1.0e10
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
 // settings
 const unsigned int SCR_WIDTH = 1000;
@@ -31,8 +32,8 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 // lighting
-glm::vec3 lightPos(0.0f, 15.0f, 0.0f);
-// glm::vec3 lightPos(1.2f, 0.0f, 0.25f);
+glm::vec3 PointlightPos(0.0f, 15.0f, 0.0f);
+// glm::vec3 PointlightPos(1.2f, 0.0f, 0.25f);
 
 const int MAXPOINT = 40000;
 const int MAXINDEX = 70000;
@@ -41,17 +42,22 @@ const char* normalFile = "bunny_normal.ply2";
 GLfloat vertices[MAXPOINT*6];
 GLuint indices[MAXINDEX*3];
 
+// Attenuation or not (for point lights)
+bool isAttenuation = false;
+bool isFlashlight = false;
 int main(){
-    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    std::cout << "-------------------------------------------------\n";
+    std::cout << "Starting GLFW context, OpenGL 3.3\n";
+    std::cout << "L: Attenuation of point light\n";
+    std::cout << "P: Flash light\n";
+    std::cout << "-------------------------------------------------\n";
     // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Tina's bunny", NULL, NULL);
     if (window == NULL){
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -67,26 +73,21 @@ int main(){
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
     // configure global opengl state
-    // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
     // build and compile our shader zprogram
-    // ------------------------------------
     Shader lightingShader("bunny.vs", "bunny.fs");
     Shader lampShader("lamp.vs", "lamp.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
     GLuint vertices_size, indices_size;
     parseNormal(normalFile, vertices, indices, vertices_size, indices_size);
-    std::cout << vertices_size << std::endl << indices_size << std::endl;
     
     float lightVertices[] = {
         -0.5f, -0.5f, -0.5f,  0.5f, -0.5f, -0.5f, 
@@ -112,10 +113,10 @@ int main(){
         -0.5f,  0.5f, -0.5f,  0.5f,  0.5f, -0.5f, 
          0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f, 
         -0.5f,  0.5f,  0.5f, -0.5f,  0.5f, -0.5f
-        };
+    };
 
     // first, configure the bunny's VAO (and VBO)
-    unsigned int bunnyVAO, bunnyVBO, bunnyEBO;//cubeVAO[0] for vertices, cubeVAO[1] for normals
+    unsigned int bunnyVAO, bunnyVBO, bunnyEBO;
     glGenVertexArrays(1, &bunnyVAO);
     glGenBuffers(1, &bunnyVBO);
     glGenBuffers(1, &bunnyEBO);
@@ -152,26 +153,21 @@ int main(){
 
 
     // render loop
-    // -----------
     while (!glfwWindowShouldClose(window)){
         // per-frame time logic
-        // --------------------
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
         // input
-        // -----
         processInput(window);
-
+        glfwSetKeyCallback(window, key_callback);
         // render
-        // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("light.position", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
 
         // light properties
@@ -184,11 +180,30 @@ int main(){
         lightColor.z = 1.0f;
         glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // decrease the influence
         glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
-        lightingShader.setVec3("light.ambient", ambientColor);
-        lightingShader.setVec3("light.diffuse", diffuseColor);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        // point light
+        lightingShader.setVec3("pointLights.position", PointlightPos);
+        lightingShader.setVec3("pointLights.ambient", ambientColor);
+        lightingShader.setVec3("pointLights.diffuse", diffuseColor);
+        lightingShader.setVec3("pointLights.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("pointLights.constant", 1.0f);
+        lightingShader.setFloat("pointLights.linear", (isAttenuation ? 0.014 : 0.0f));
+        lightingShader.setFloat("pointLights.quadratic", (isAttenuation ? 0.0007 : 0.0f));
+        
+        // spotLight
+        lightingShader.setVec3("spotLight.position", camera.Position);
+        lightingShader.setVec3("spotLight.direction", camera.Front);
+        lightingShader.setVec3("spotLight.ambient", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("spotLight.constant", (isFlashlight? 1.0f : INF));
+        lightingShader.setFloat("spotLight.linear", 0.045f);
+        lightingShader.setFloat("spotLight.quadratic", 0.0075f);
+        lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(5.0f)));
+        lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(9.0f)));     
 
         // material properties
+        //lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f); // specular lighting doesn't have full effect on this object's material
@@ -216,39 +231,33 @@ int main(){
         lampShader.setMat4("projection", projection);
         lampShader.setMat4("view", view);
         model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
+        model = glm::translate(model, PointlightPos);
         model = glm::scale(model, glm::vec3(0.4f)); // a smaller cube
         lampShader.setMat4("model", model);
 
         glBindVertexArray(lightVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &bunnyVAO);
     glDeleteVertexArrays(1, &lightVAO);
     glDeleteBuffers(1, &bunnyVBO);
     glDeleteBuffers(1, &lightVBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window){
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -258,9 +267,21 @@ void processInput(GLFWwindow *window){
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
-
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode){
+    if (key == GLFW_KEY_L && action == GLFW_PRESS){
+        isAttenuation = !isAttenuation;
+        std::cout << "Point light's attenuation is ";
+        if(isAttenuation) std::cout << "on.\n";
+        else std::cout << "off.\n";
+    }
+    if (key == GLFW_KEY_P && action == GLFW_PRESS){
+        isFlashlight = !isFlashlight;
+        std::cout << "Flash light is ";
+        if(isFlashlight) std::cout << "on.\n";
+        else std::cout << "off.\n";
+    }
+}
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
@@ -269,25 +290,20 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
 
 // glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos){
     if (firstMouse){
         lastX = xpos;
         lastY = ypos;
         firstMouse = false;
     }
-
     float xoffset = xpos - lastX;
     float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
     lastX = xpos;
     lastY = ypos;
-
     camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
     camera.ProcessMouseScroll(yoffset);
 }
